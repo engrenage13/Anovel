6d6618068013b5ff053bfeac630c2bb870cb1b3b
engrenage <severacval@gmail.com>
2022-06-29 18:08:01 +0200

    Ajout d'un fond d'écran sur le menu principal et suppression de l'effet particule

--- Navale.py
+++ Navale.py
@@ -1,6 +1,5 @@
 from systeme.FondMarin import *
 from partie import Partie
-from animations.Paillette import Paillette
 from ui.bouton import Bouton
 from systeme.fenetre import Fenetre
 
@@ -16,19 +15,17 @@ quit = Bouton([fen.switchEtat], "Quitter", [DARKGRAY, GRAY, WHITE])
 nanav = load_image('images/logos/Navale.png')
 image_resize(nanav, int(nanav.width*0.19), int(nanav.height*0.19))
 logo = load_texture_from_image(nanav)
+tableau = load_image('images/backgrounds/valley.png')
+ratio = yf/tableau.height
+image_resize(tableau, int(tableau.width*ratio), int(tableau.height*ratio))
+fond = load_texture_from_image(tableau)
 
 def accueil():
-    draw_rectangle_gradient_v(0, 0, xf, yf, (0, 0, 60, 200), (0, 0, 30, 100))
-    for i in range(len(lolo)):
-        lolo[i].dessine()
+    draw_texture(fond, 0, 0, WHITE)
     draw_texture(logo, 0, 0, WHITE)
     draw_text_pro(police2, version, (int(xf*0.005), int(yf*0.975)), (0, 0), 0, 21, 0, WHITE)
     start.dessine((int(xf*0.35), int(yf*0.92)))
     quit.dessine((int(xf*0.65), int(yf*0.92)))
-
-lolo = []
-for i in range(10):
-    lolo.append(Paillette((0, 0, xf, yf), [BLUE, DARKBLUE, SKYBLUE]))
 
 while not fen.jeuDoitFermer():
     begin_drawing()
