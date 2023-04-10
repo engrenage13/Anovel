0c484d7adb4f4a1780a930bfeabb9c9c5d18d4ff
engrenage <severacval@gmail.com>
2023-04-10 11:39:05 +0200

    Refonte de la classe Element

--- jeux/Jeu_1/objets/element.py
+++ jeux/Jeu_1/objets/element.py
@@ -2,28 +2,27 @@ from systeme.FondMarin import *
 from jeux.Jeu_1.fonctions.jeu import TAILLECASE
 
 class Element:
+    """L'objet de base pour générer n'importe quel élément du jeu.
+    """
     def __init__(self, image: str, x: int = 0, y: int = 0) -> None:
         self.pos = (x, y)
-        # Images
-        originale = load_image(image)
-        image_resize(originale, int(originale.width*TAILLECASE/originale.width), 
-                     int(originale.height*TAILLECASE/originale.width))
-        self.originale = originale
-        self.image = load_texture_from_image(self.originale)
-        unload_image(originale)
-        # /Images
-        self.dimensions = (self.image.width, self.image.height)
-        self.coords = [int(x-self.dimensions[0]/2), int(y-self.dimensions[1]/2), 
-                       self.image.width, self.image.height]
+        self.setImage(image)
 
     def dessin(self) -> None:
+        """Permet d'afficher l'élément
+        """
         x = self.pos[0]
         y = self.pos[1]
-        l = self.dimensions[0]
-        h = self.dimensions[1]
+        l = self.dims[0]
+        h = self.dims[1]
         draw_texture(self.image, int(x-l/2), int(y-h/2), WHITE)
 
     def getContact(self) -> bool:
+        """Vérifie si le pointeur de la souris est sur l'élément
+
+        Returns:
+            bool: True si la souris est sur l'élément
+        """
         rep = False
         x = get_mouse_x()
         y = get_mouse_y()
@@ -31,3 +30,35 @@ class Element:
             if y >= self.coords[1] and y <= self.coords[1]+self.coords[3]:
                 rep = True
         return rep
+    
+    def setPos(self, x: int, y: int) -> None:
+        """Change la position de l'élément
+
+        Args:
+            x (int): Le nouvel abscisse de l'élément
+            y (int): Le nouvel ordonnée de l'élément
+        """
+        self.pos = (x, y)
+        self.setCoords()
+        
+    def setImage(self, image: str) -> None:
+        """Change l'image de l'élément
+
+        Args:
+            image (str): La nouvelle image pour l'élément
+        """
+        originale = load_image(image)
+        image_resize(originale, int(originale.width*TAILLECASE/originale.width), 
+                     int(originale.height*TAILLECASE/originale.width))
+        self.originale = originale
+        self.image = load_texture_from_image(self.originale)
+        unload_image(originale)
+        self.dims = (self.image.width, self.image.height)
+        self.setCoords()
+
+    def setCoords(self) -> None:
+        """Modifie les coordonnées de l'élément
+        """
+        x = self.pos[0]
+        y = self.pos[1]
+        self.coords = [int(x-self.dims[0]/2), int(y-self.dims[1]/2), self.image.width, self.image.height]
