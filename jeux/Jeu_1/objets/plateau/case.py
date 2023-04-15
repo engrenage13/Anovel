0570a6ebf4100a5a91d3d0650835226323f2c4c1
engrenage <severacval@gmail.com>
2023-04-15 07:45:56 +0200

    Ajout d'un espace pour un envirronement et de nouvelles couleurs pour les cases

--- jeux/Jeu_1/objets/plateau/case.py
+++ jeux/Jeu_1/objets/plateau/case.py
@@ -1,13 +1,15 @@
+import random
 from systeme.FondMarin import *
-from jeux.Jeu_1.fonctions.bases import TAILLECASE
+from jeux.Jeu_1.fonctions.bases import TAILLECASE, EAUX
 
 class Case:
-    def __init__(self, x: int = 0, y: int = 0, taille: int = TAILLECASE) -> None:
+    def __init__(self, x: int = 0, y: int = 0) -> None:
         self.pos = (x, y)
-        self.taille = taille
+        self.taille = TAILLECASE
+        self.couleur = random.choice(EAUX)
 
     def dessine(self) -> None:
-        draw_rectangle(self.pos[0], self.pos[1], self.taille, self.taille, [48, 140, 201, 255])
+        draw_rectangle(self.pos[0], self.pos[1], self.taille, self.taille, self.couleur)
         draw_rectangle_lines_ex([self.pos[0], self.pos[1], self.taille, self.taille], 1.5, [80, 80, 80, 150])
 
     def deplace(self, x: int, y: int) -> None:
--- jeux/Jeu_1/objets/plateau/plateau.py
+++ jeux/Jeu_1/objets/plateau/plateau.py
@@ -1,25 +1,27 @@
-from systeme.FondMarin import xf, yf, draw_rectangle_lines_ex, draw_rectangle
+from systeme.FondMarin import *
 from jeux.Jeu_1.objets.plateau.case import Case, TAILLECASE
 
 class Plateau:
     def __init__(self, nbCases: int) -> None:
         self.nbCases = nbCases
         self.largeurBordure = int(yf*0.05)
+        self.largeurEnvirronement = int(yf*0.2)
         self.cases = []
-        x = self.largeurBordure
-        y = self.largeurBordure
+        x = self.largeurBordure+self.largeurEnvirronement
+        y = self.largeurBordure+self.largeurEnvirronement
         for i in range(nbCases):
             cases = []
             for j in range(nbCases):
                 cases.append(Case(x, y))
                 x += TAILLECASE
             self.cases.append(cases)
-            x = self.largeurBordure
+            x = self.largeurBordure+self.largeurEnvirronement
             y += TAILLECASE
         # Lien Menu
         self.bloque = False
 
     def dessine(self) -> None:
+        self.dessineEnvirronement()
         self.dessineBordure()
         for i in range(self.nbCases):
             for j in range(self.nbCases):
@@ -31,11 +33,17 @@ class Plateau:
         ajustFin = l*2
         OR = [169, 142, 23, 255]
         draw_rectangle_lines_ex([p[0]-l, p[1]-l, TAILLECASE*self.nbCases+ajustFin, 
-                                 TAILLECASE*self.nbCases+ajustFin], l, [11, 23, 62, 255])
+                                 TAILLECASE*self.nbCases+ajustFin], l, BLACK)
         draw_rectangle(p[0]-l, p[1]-l, l*2, l*2, OR)
         draw_rectangle(p[0]-l+TAILLECASE*self.nbCases, p[1]-l, l*2, l*2, OR)
         draw_rectangle(p[0]-l, p[1]-l+TAILLECASE*self.nbCases, l*2, l*2, OR)
         draw_rectangle(p[0]-l+TAILLECASE*self.nbCases, p[1]-l+TAILLECASE*self.nbCases, l*2, l*2, OR)
+
+    def dessineEnvirronement(self) -> None:
+        p = self.cases[0][0].pos
+        l = self.largeurEnvirronement+self.largeurBordure
+        ajustFin = l*2
+        draw_rectangle(p[0]-l, p[1]-l, TAILLECASE*self.nbCases+ajustFin, TAILLECASE*self.nbCases+ajustFin, [11, 23, 62, 255])
 
     def deplace(self, x: int, y: int) -> None:
         if not self.passeFrontiereHorizontale(x) and self.passeFrontiereVerticale(y):
@@ -50,16 +58,16 @@ class Plateau:
 
     def passeFrontiereHorizontale(self, x: int) -> bool:
         rep = False
-        if self.cases[0][0].pos[0]+x > self.largeurBordure:
+        if self.cases[0][0].pos[0]+x > self.largeurBordure+self.largeurEnvirronement:
             rep = True
-        elif self.cases[0][self.nbCases-1].pos[0]+TAILLECASE+x < xf-self.largeurBordure:
+        elif self.cases[0][self.nbCases-1].pos[0]+TAILLECASE+x < xf-self.largeurBordure-self.largeurEnvirronement:
             rep = True
         return rep
     
     def passeFrontiereVerticale(self, y: int) -> bool:
         rep = False
-        if self.cases[0][0].pos[1]+y > self.largeurBordure:
+        if self.cases[0][0].pos[1]+y > self.largeurBordure+self.largeurEnvirronement:
             rep = True
-        elif self.cases[self.nbCases-1][0].pos[1]+TAILLECASE+y < yf-self.largeurBordure:
+        elif self.cases[self.nbCases-1][0].pos[1]+TAILLECASE+y < yf-self.largeurBordure-self.largeurEnvirronement:
             rep = True
         return rep
