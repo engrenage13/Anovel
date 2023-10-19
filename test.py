da70ecf6770b51403632db17ba9f494adde99aa9
engrenage <severacval@gmail.com>
2023-10-19 22:01:02 +0200

    Ajout de la logique d'enfants aux composant flex

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-from systeme.FondMarin import begin_drawing, end_drawing, clear_background, BLACK, BLUE
+from systeme.FondMarin import *
 from systeme.fenetre import Fenetre
 from ui.flex import Flex
 
 fen = Fenetre()
 base = Flex()
-f1 = Flex("38%", 222)
+f1 = Flex(("38%", xf), (222, yf))
+f1.ajoute_enfant(h="40%", couleur=ORANGE)
+f1.ajoute_enfant("40%", 43, RED)
+base.enfants.append((f1, BLUE))
 
 while not fen.jeuDoitFermer():
     begin_drawing()
     clear_background(BLACK)
     base.dessine()
-    f1.dessine(BLUE)
     end_drawing()
 
 fen.finDuJeu()
--- ui/flex.py
+++ ui/flex.py
@@ -1,14 +1,17 @@
 from systeme.FondMarin import *
 
 class Flex:
-    def __init__(self, l: int|str = "", h: int|str = "") -> None:
-        self.longueur = self.definiTaille(l, xf)
-        self.hauteur = self.definiTaille(h, yf)
+    def __init__(self, l: tuple[int|str, int] = ("", xf), h: tuple[int|str, int] = ("", yf)) -> None:
+        self.longueur = self.defini_taille(l[0], l[1])
+        self.hauteur = self.defini_taille(h[0], h[1])
+        self.enfants = []
 
-    def dessine(self, couleur: Color = [0, 0, 0, 0]) -> None:
+    def dessine(self, couleur: Color = BLANK) -> None:
         draw_rectangle(0, 0, self.longueur, self.hauteur, couleur)
+        for i in range(len(self.enfants)):
+            self.enfants[i][0].dessine(self.enfants[i][1])
 
-    def definiTaille(self, l: int|str, dimMax: int) -> int:
+    def defini_taille(self, l: int|str, dimMax: int) -> int:
         if type(l) == int:
             rep = l
         elif type(l) == str:
@@ -18,3 +21,6 @@ class Flex:
                 m = int(l[0:-1])/100
                 rep = int(dimMax*m)
         return rep
+    
+    def ajoute_enfant(self, l: int|str = "", h: int|str = "", couleur: Color = BLANK) -> None:
+        self.enfants.append((Flex((l, self.longueur), (h, self.hauteur)), couleur))
