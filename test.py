17783fb3f7b8f27dd374d99c4a01bd303ca372b7
engrenage <severacval@gmail.com>
2023-11-04 16:47:35 +0100

    Ajout d'un paramètre pour la direction et d'un autre pour la marge intérieure

--- test.py
+++ test.py
@@ -3,16 +3,17 @@ from systeme.fenetre import Fenetre
 from ui.flex import Flex
 
 fen = Fenetre()
-base = Flex()
-f1 = Flex(("38%", xf), (222, yf))
+base = Flex(pad=20)
+f1 = Flex(("38%", base.longueur), (222, base.hauteur), 1)
 f1.ajoute_enfant(h="40%", couleur=ORANGE)
-f1.ajoute_enfant("40%", 43, RED)
+f1.ajoute_enfant("40%", 43, couleur=RED)
 base.enfants.append((f1, BLUE))
+#base.ajoute_enfant("10%", "10%", couleur=GREEN)
 
 while not fen.jeuDoitFermer():
     begin_drawing()
     clear_background(BLACK)
-    base.dessine()
+    base.dessine(couleur=[30, 30, 30, 255])
     end_drawing()
 
 fen.finDuJeu()
--- ui/flex.py
+++ ui/flex.py
@@ -1,15 +1,25 @@
 from systeme.FondMarin import *
 
 class Flex:
-    def __init__(self, l: tuple[int|str, int] = ("", xf), h: tuple[int|str, int] = ("", yf)) -> None:
+    def __init__(self, l: tuple[int|str, int] = ("", xf), h: tuple[int|str, int] = ("", yf), de: int = 0, pad: int = 0) -> None:
         self.longueur = self.defini_taille(l[0], l[1])
         self.hauteur = self.defini_taille(h[0], h[1])
+        self.padding = pad
+        self.direction = de
         self.enfants = []
 
-    def dessine(self, couleur: Color = BLANK) -> None:
-        draw_rectangle(0, 0, self.longueur, self.hauteur, couleur)
+    def dessine(self, x: int = 0, y: int = 0, couleur: Color = BLANK) -> None:
+        pad = self.padding
+        draw_rectangle(x, y, self.longueur, self.hauteur, couleur)
+        nouv_x = x+pad
+        nouv_y = y+pad
         for i in range(len(self.enfants)):
-            self.enfants[i][0].dessine(self.enfants[i][1])
+            if i > 0:
+                if self.direction == 0:
+                    nouv_y += self.enfants[i-1][0].get_dims()[1]
+                else:
+                    nouv_x += self.enfants[i-1][0].get_dims()[0]
+            self.enfants[i][0].dessine(nouv_x, nouv_y, self.enfants[i][1])
 
     def defini_taille(self, l: int|str, dimMax: int) -> int:
         if type(l) == int:
@@ -22,5 +32,8 @@ class Flex:
                 rep = int(dimMax*m)
         return rep
     
-    def ajoute_enfant(self, l: int|str = "", h: int|str = "", couleur: Color = BLANK) -> None:
-        self.enfants.append((Flex((l, self.longueur), (h, self.hauteur)), couleur))
+    def ajoute_enfant(self, l: int|str = "", h: int|str = "", de: int = 0, pad: int = 0, couleur: Color = BLANK) -> None:
+        self.enfants.append((Flex((l, self.longueur-self.padding*2), (h, self.hauteur-self.padding*2), de, pad), couleur))
+
+    def get_dims(self) -> tuple[int]:
+        return (self.longueur, self.hauteur)
